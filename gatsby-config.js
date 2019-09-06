7f78865fcd2c9cb5790325eca4849a3be50808e0
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2019-09-06 23:46:18 +0200

    Added gatsby-source-drupal plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://gatsby.docker.localhost:8000/`,
+        apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
